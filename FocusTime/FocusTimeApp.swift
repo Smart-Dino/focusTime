652a3c0af693c2c618678683066facf7c619403d
George Kyrylenko <dinozawrua@gmail.com>
2025-04-16 23:40:42 +0200

    init

---
+++ FocusTime/FocusTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimeApp.swift
+//  FocusTime
+//
+//  Created by George Kyrylenko on 16.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
