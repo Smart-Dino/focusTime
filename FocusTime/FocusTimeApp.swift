8c4bf889375dc630d2496fde8794c617bfdeebc6
Maksym Horobets <makswinters@outlook.com>
2025-06-02 17:16:13 +0300

    Commented debug view for now

--- FocusTime/FocusTimeApp.swift
+++ FocusTime/FocusTimeApp.swift
@@ -11,12 +11,12 @@ import SwiftUI
 struct FocusTimeApp: App {
     var body: some Scene {
         WindowGroup {
-            // MARK: This will get removed as soon as everyting is tested
-            #if(DEBUG)
-            StoreKitPaymentManagerDebugView(paymentManager: StoreKitPaymentManager())
-            #else
+              // MARK: This will get removed as soon as everyting is tested
+//            #if(DEBUG)
+//            StoreKitPaymentManagerDebugView(paymentManager: StoreKitPaymentManager())
+//            #else
             ContentView()
-            #endif
+//            #endif
         }
     }
 }
