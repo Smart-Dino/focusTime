a60562ab4fb3a776a5445f80108fbecd14c5fbdc
Maksym Horobets <makswinters@outlook.com>
2025-07-14 16:06:46 +0300

    Updated TimeComponents with new, much more useful APIs

--- FocusTime/BusinessLogic/Models/Blocking/Schedule/Helpers/TimeComponents.swift
+++ FocusTime/BusinessLogic/Models/Blocking/Schedule/Helpers/TimeComponents.swift
@@ -12,8 +12,47 @@ import Foundation
 // https://fatbobman.com/en/posts/considerations-for-using-codable-and-enums-in-swiftdata-models/#:~:text=Such%20errors%20indicate,of%20the%20model.
 /// A simplified alternative to `DateComponents` that works with SwiftData.
 /// Represents a time of day in hours and minutes, stored as seconds since midnight (00:00:00 UTC).
-struct TimeComponents: Codable, Hashable, CustomStringConvertible {
-    let timeSince1970: Int
+nonisolated struct TimeComponents: Codable, Hashable, CustomStringConvertible {
+    private let timeSince1970: Int
+    
+    /// Seconds since midnight in the current locale.
+    var localizedTimeSince1970: Int {
+        let dateUTC0 = Date(timeIntervalSince1970: TimeInterval(timeSince1970))
+        let secondsInCurrentLocale = Calendar.current.component(.second, from: dateUTC0)
+        
+        return secondsInCurrentLocale
+    }
+    
+    /// Returns localized time string in the user's current calendar and time zone,
+    /// formatted with the hour and minute components (e.g. "8:30 AM" or "20:30").
+    var description: String {
+        // Prepare formatter.
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .none
+        
+        let date = Calendar.current.date(from: dateComponents) ?? Date()
+        
+        // Return formatted string.
+        return formatter.string(from: date)
+    }
+    
+    /// Provides the hour and minute components for the stored time.
+    var dateComponents: DateComponents {
+        // Prepare calendar.
+        var utcCalendar = Calendar.current
+        utcCalendar.timeZone = .gmt // We use GMT+0 since timeIntervalSince1970 is initialized relative to 00:00:00 UTC.
+        
+        let date = Date(timeIntervalSince1970: TimeInterval(timeSince1970))
+        
+        // Create and return components.
+        return utcCalendar.dateComponents([.hour, .minute], from: date)
+    }
+    
+    /// Localized Date using the stored hour and minute.
+    var localizedDate: Date {
+        Calendar.current.date(from: dateComponents) ?? Date()
+    }
     
     /// Initializes with seconds since midnight (00:00:00 UTC).
     /// - Parameter secondsSince1970: Number of seconds since midnight; must be in range 0..<86400.
@@ -42,32 +81,6 @@ struct TimeComponents: Codable, Hashable, CustomStringConvertible {
               let minute = components.minute else { return nil }
         
         self.init(hour: hour, minute: minute)
-    }
-
-    /// Returns localized time string in the user's current calendar and time zone,
-    /// formatted with the hour and minute components (e.g. "8:30 AM" or "20:30").
-    var description: String {
-        // Prepare formatter.
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        formatter.dateStyle = .none
-        
-        let date = Calendar.current.date(from: dateComponents) ?? Date()
-        
-        // Return formatted string.
-        return formatter.string(from: date)
-    }
-    
-    /// Provides the hour and minute components for the stored time.
-    var dateComponents: DateComponents {
-        // Prepare calendar.
-        var utcCalendar = Calendar.current
-        utcCalendar.timeZone = .gmt // We use GMT+0 since timeIntervalSince1970 is initialized relative to 00:00:00 UTC.
-        
-        let date = Date(timeIntervalSince1970: TimeInterval(timeSince1970))
-        
-        // Create and return components.
-        return utcCalendar.dateComponents([.hour, .minute], from: date)
     }
 }
 
