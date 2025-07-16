ac224e5a4ea79bebccf8f76a38771bc5a43d1ae8
Maksym Horobets <makswinters@outlook.com>
2025-07-16 14:36:33 +0300

    StoreKit ftUnit extension is now safer

--- FocusTime/Extentions/StoreKit+Ext.swift
+++ FocusTime/Extentions/StoreKit+Ext.swift
@@ -8,13 +8,13 @@
 import StoreKit
 
 extension StoreKit.Product.SubscriptionPeriod.Unit {
-    var ftUnit: PeriodConverter.Unit {
+    var ftUnit: PeriodConverter.Unit? {
         switch self {
         case .day: .day
         case .week: .week
         case .month: .month
         case .year: .year
-        @unknown default: fatalError("Unknown subscription period unit")
+        @unknown default: nil
         }
     }
 }
